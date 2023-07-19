1f91a30bef20b9d0456caa558f8578380845361c
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-19 16:20:58 +0100

    faiss support done

--- zerorank/utility/build_index.py
+++ zerorank/utility/build_index.py
@@ -2,9 +2,8 @@ import faiss
 from fire import Fire
 from os.path import join
 import numpy as np
-import json
 
-def main(artifact_dir : str, query_only : bool = False, nprobe : int = 10, nlist : int = 100, niter : int = 5, n : int = 10, quantized_dim : int = 256):
+def main(artifact_dir : str, output : str, query_only : bool = False, quantized_dim : int = 256, ncoarse : int = 100):
     queries = np.load(join(artifact_dir, 'q_embeddings.npy'))
     if not query_only:
         docs = np.load(join(artifact_dir, 'd_embeddings.npy'))
@@ -12,12 +11,18 @@ def main(artifact_dir : str, query_only : bool = False, nprobe : int = 10, nlist
     else:
         embeddings = queries
 
-    lookup = np.load(join(artifact_dir, 'idx.npy'))
+    idx = np.load(join(artifact_dir, 'idx.npy'))
 
-    coarse_quantizer = faiss.IndexFlatL2 (quantized_dim)
-    sub_index = faiss.IndexIVFPQ (coarse_quantizer, quantized_dim, ncoarse, 16, 8)
-    
+    pca_matrix = faiss.PCAMatrix(embeddings.shape[-1], quantized_dim, 0, False) 
+    coarse_quantizer = faiss.IndexFlatL2(quantized_dim)
+    sub_index = faiss.IndexIVFPQ(coarse_quantizer, quantized_dim, ncoarse, 16, 8)
 
+    index = faiss.IndexPreTransform(pca_matrix, sub_index)
+    index.train(embeddings)
+    index.add_with_ids(embeddings, idx)
+
+    identifier = 'q' if query_only else 'qd'
+    faiss.write_index(index, join(output, f'index_{identifier}_{quantized_dim}_{ncoarse}.index'))
 
 if __name__ == "__main__":
     Fire(main)
