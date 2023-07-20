4bab201b435bd169bd56a4170c93f1f545afc807
Andrew Parry <2393265p@student.gla.ac.uk>
2023-07-20 15:41:44 +0100

    nervous breakdown iminent

--- zerorank/utility/subset.py
+++ zerorank/utility/subset.py
@@ -3,11 +3,10 @@ import ir_datasets as irds
 import pandas as pd
 from os.path import join
 
-def main(dataset : str = 'msmarco-passage',
+def main(dataset : str = '/data/triples.tsv',
          output_dir : str = '/',
          cutoff : int = 1000000):
-    ds = irds.load(dataset)
-    docpairs = pd.DataFrame(ds.docpairs_iter())
+    docpairs = pd.read_csv(dataset, sep='\t', header=None, names=['query_id', 'doc_id_a', 'doc_id_b'], dtype={'query_id' : str, 'doc_id_a' : str, 'doc_id_b' : str})
     docpairs = docpairs.drop_duplicates(subset=['query_id'])
     subset = docpairs.sample(n=cutoff).reset_index(drop=True)
     subset.to_csv(join(output_dir, f'subset{cutoff}.tsv'), sep='\t', index=False, header=False)
